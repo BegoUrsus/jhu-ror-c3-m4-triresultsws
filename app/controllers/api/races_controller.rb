4ef7f02c2a5864b8e12a27c7ee1f7311a26c90bf
BegoUrsus <layira2002@gmail.com>
2016-05-21 18:39:48 +0200

    Methods - 1

--- app/controllers/api/races_controller.rb
+++ app/controllers/api/races_controller.rb
@@ -29,10 +29,31 @@ module Api
 				render plain: "#{params[:race][:name]}", status: :ok
 			else
 				#real implementation
+				@race = Race.new(race_params);
+				if @race.save
+					render plain: race_params[:name], status: :created
+				else
+					render plain: "couldn't save"
+				end
 			end
-
 		end
 
+	  # PATCH/PUT /api/races/1
+	  # PATCH/PUT /api/races/1.json
+	  def update
+	  end
+
+	  # DELETE /api/races/1
+	  # DELETE /api/races/1.json
+	  def destroy
+	  end
+
+
+	  private
+      # Never trust parameters from the scary internet, only allow the white list through.
+	    def race_params
+	      params.require(:race).permit(:name, :date)
+	    end
 
 	end
 end
