dc373fa78663226324505d7496cdc61cc8e641bf
BegoUrsus <layira2002@gmail.com>
2016-05-22 11:29:11 +0200

    Resource Implementation (JSON) - 3

--- app/controllers/api/results_controller.rb
+++ app/controllers/api/results_controller.rb
@@ -2,6 +2,7 @@
 module Api
 	class ResultsController < ApplicationController
 
+
 	  # GET api/races/:race_id/results
 	  # GET api/races/:race_id/results.json	  
 	  def index
@@ -10,7 +11,7 @@ module Api
 			else
 				#real implementation ...
 			end
-	  end
+	  end #index
 
 	  # GET api/races/:race_id/results/1
 	  # GET api/races/:race_id/results/1.json
@@ -22,7 +23,41 @@ module Api
 				@result=Race.find(params[:race_id]).entrants.where(:id=>params[:id]).first
 				render :partial=>"result", :object=>@result
 			end
-	  end
+	  end #show
 
-	end
-end
+	  # PATCH/PUT /api/races/:race_id/results/1
+	  # PATCH/PUT /api/races/:race_id/results/1json
+	  def update
+	  	# 	Rails.logger.debug("method=#{request.method}")
+	  	# 	request.method can be 'PATCH' or 'PUT'
+	  	entrant = Race.find(params[:race_id]).entrants.where(:id=>params[:id]).first
+			result = params[:result]
+
+			if result
+				if result[:swim]
+					entrant.swim=entrant.race.race.swim
+					entrant.swim_secs = result[:swim].to_f
+				end
+				if result[:t1]
+					entrant.t1=entrant.race.race.t1
+					entrant.t1_secs = result[:t1].to_f
+				end
+				if result[:bike]
+					entrant.bike=entrant.race.race.bike
+					entrant.bike_secs = result[:bike].to_f
+				end
+				if result[:t2]
+					entrant.t2=entrant.race.race.t2
+					entrant.t2_secs = result[:t2].to_f
+				end
+				if result[:run]
+					entrant.run=entrant.race.race.run
+					entrant.run_secs = result[:run].to_f
+				end
+				entrant.save
+				render plain: :nothing, status: :ok
+			end #result
+	  end #update
+
+	end #ResultsController
+end #Api
